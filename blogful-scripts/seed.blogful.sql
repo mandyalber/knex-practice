eee4bf1116eb7c1df5776ab5207ce8b0738e89e2
Mandy Alber <mandy.alber@gmail.com>
2020-10-30 10:59:57 -0700

    completed checkpoint drills

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,24 @@
+INSERT INTO blogful_articles (title, date_published, content)
+VALUES
+    ('Fish tricks', now() - '20 days'::INTERVAL, 'here is some fake article content'),
+    ('Anywho', now() - '19 days'::INTERVAL, 'tralalalala hkde ekkht ekgoit eiongl eiin'),
+    ('Bubbles are Cool', now() - '18 days'::INTERVAL, 'did you know that bubbles are very cool'),
+    ('First words', now() - '17 days'::INTERVAL, 'mama dada baba nite nite up bye bye hi yeah eat more'),
+    ('How to be Creative', now() - '16 days'::INTERVAL, 'I am not very creative. please tell be how to be'),
+    ('Another Article', now() - '15 days'::INTERVAL, 'angh  aihtahfkj eihooknfa oehnohga hoeingoiahbg'),
+    ('Yeah', now() - '14 days'::INTERVAL, 'Yeah yeah yeah yeah yeah yeah yeah ayea'),
+    ('GobbledyGook', now() - '13 days'::INTERVAL, 'ah;gk akng;lk ahio;ogna; ghario;hgnnga haklfn anlkngd'),
+    ('LabDob', now() - '12 days'::INTERVAL, 'AKLH AGA KALG HAL; AHGKL;A NG;LKA OERI; NG NG AKL'),
+    ('Ha Ha', now() - '11 days'::INTERVAL, 'abg;k a ankgl a;hoin ahkl; alk jankfkjg najk; k;lfda ngn'),
+    ('Lorem', now() - '10 days'::INTERVAL, 'ahrta uyzhgjjxzfj yzdthxgh jzfhhf kl ankgan naklag ;klf '),
+    ('Ipsum', now() - '9 days'::INTERVAL, 'Can i just get my cat to walk across the keyboard or something'),
+    ('Dolor', now() - '8 days'::INTERVAL, 'I''m a little marty and my mommy loves me. '),
+    ('Imet', now() - '7 days'::INTERVAL, 'here is some fake article content'),
+    ('Something', now() - '6 days'::INTERVAL, 'The wheels on the bus go round and round, round and round'),
+    ('Like', now() - '5 days'::INTERVAL, 'Old McDonald had a farm, eieio. And on that farm he had a ????'),
+    ('That', now() - '4 days'::INTERVAL, 'woof wwooof wooof owoowfn ow fnowofnwno owfowo fwoowo wofowjowfj'),
+    ('I', now() - '3 days'::INTERVAL, 'jajajajajjajajajjajajajajajjajajjajajajjajajajjajajajjajajajjajajj'),
+    ('Think', now() - '2 days'::INTERVAL, 'Oh there was a farmer had a dog and bingo was his name o'),
+    ('Ooh La La', now() - '1 days'::INTERVAL, 'B I N G O, B I N G O, B I N G O, and bingo was his name o')
+
+;
